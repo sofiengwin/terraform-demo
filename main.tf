95ff5fa21b359a2b58309c58d5a7509d8888f698
sofiengwin <godwin.onisofien@andela.com>
2021-12-05 16:59:12 -0800

    functions

--- main.tf
+++ main.tf
@@ -27,19 +27,62 @@ provider "aws" {
   region  = var.main_region
 }
 
+# module "vpc" {
+#   source = "./modules/vpc"
+#   region = var.main_region
+# }
+
 module "vpc" {
-  source = "./modules/vpc"
-  region = var.main_region
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "terraform"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["us-east-1a"]
+  public_subnets = ["10.0.1.0/24"]
+
+
+}
+
+
+resource "aws_security_group" "my-sg" {
+  vpc_id = module.vpc.vpc_id
+  name   = join("_", ["sg", module.vpc.vpc_id])
+  dynamic "ingress" {
+    for_each = var.rules
+    content {
+      from_port   = ingress.value["port"]
+      to_port     = ingress.value["port"]
+      protocol    = ingress.value["proto"]
+      cidr_blocks = ingress.value["cidr_blocks"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Terraform-Dynamic-SG"
+  }
 }
 
 resource "aws_instance" "app_server" {
-  ami           = module.vpc.ami_id
-  subnet_id     = module.vpc.subnet_id
-  instance_type = "t2.micro"
+  ami             = data.aws_ssm_parameter.this.value
+  subnet_id       = module.vpc.public_subnets[0]
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.my-sg.id]
+  user_data       = fileexists("script.sh") ? file("script.sh") : null
 
   tags = {
     Name = var.instance_name
   }
+}
+
+data "aws_ssm_parameter" "this" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
 variable "instance_name" {
--- output.tf
+++ output.tf
@@ -2,3 +2,13 @@ output "PrivateIP" {
   description = "Private IP of EC2 instance"
   value       = aws_instance.app_server.private_ip
 }
+
+output "Web-Server-URL" {
+  description = "Web-Server-URL"
+  value       = join("", ["http://", aws_instance.app_server.public_ip])
+}
+
+output "Time-Date" {
+  description = "Date/Time of Execution"
+  value       = timestamp()
+}
